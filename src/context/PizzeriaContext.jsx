371a96472af76958df4a1e577df9fdcd7a75a7fb
Chokis <mapaulaquintero@gmail.com>
2024-05-25 15:22:35 -0500

    Context change

--- src/context/PizzeriaContext.jsx
+++ src/context/PizzeriaContext.jsx
@@ -12,22 +12,29 @@ export const PizzeriaProvider = ({ children }) => {
         setGetPizzas(data);
     }
 
-    const addToCart = (id,price,name,img) => {
+    const addToCart = ({id,price,name,img}) => {
         const indexPizza = cart.findIndex(p => p.id == id)
-        const product = {id,price,name,img,count:1}
- 
+        console.log(cart, 'cart')
+        console.log(indexPizza, 'indexPizza')
+
+        const product = { id, price, name, img, count: 1 }
+        console.log(product, "productContext")
+
         if(indexPizza >= 0) {
+        //El producto ya existe en el carrito, por lo tanto se incrementa la cantidad del mismo
             cart[indexPizza].count++
             setCart([...cart])
         }else {
+        //El producto no existe, por lo tanto se agrega al carrito
             setCart([...cart,product])
+            console.log(cart, 'cart')
         }
         console.log(cart)
     }
 
     useEffect(() => {
         allPizzas();
-    },[])
+    }, [])
 
     return (
     <PizzeriaContext.Provider value={{ getPizzas, cart, setGetPizzas, addToCart}}>
