958aecee46ff7a1cc328f6dda1af23c91c8c9d34
paulawon <166189769+paulawon@users.noreply.github.com>
2024-05-26 21:27:38 -0400

    Update cart view

--- src/views/Cart.jsx
+++ src/views/Cart.jsx
@@ -1,14 +1,13 @@
-import {useContext} from 'react';
+import { useContext } from 'react';
 import { PizzeriaContext } from '../context/PizzeriaContext';
 
 const Cart = () => {
-    const {cart, total} = useContext(PizzeriaContext);
-    console.log(total, "total")
+  const { cart, addItem, removeItem } = useContext(PizzeriaContext);
 
-    const totalAmount = cart.reduce(
-      (accumulator, initialValue) => accumulator + (initialValue.count * initialValue.price), 0
-    )
-    console.log(totalAmount, "totalAmount")
+  const totalAmount = cart.reduce(
+    (accumulator, initialValue) => accumulator + (initialValue.count * initialValue.price), 0
+  )
+  console.log(totalAmount, "totalAmount")
 
   return (
     <div>
@@ -17,17 +16,26 @@ const Cart = () => {
       </div>
       <h4>Detalles</h4>
       {
-        cart && cart.map((product)=> (
-            <div className='d-flex justify-content-around' key={product.id}>
-                <div>
-                    <img src={product.img} alt="Foto pizza" />
-                    <h3>{product.name}</h3>
-                </div>
-                <div>
-                    <span>{product.count * product.price}</span>
-                    <span>{product.count}</span>
-                </div>
+        cart && cart.map((product, index) => (
+          <div className='d-flex justify-content-around' key={product.id}>
+            <div className='d-flex'>
+              <img src={product.img} alt="Foto pizza" width="50px" />
+              <p>{product.name}</p>
             </div>
+            <div>
+              <span>{product.count * product.price}</span>
+              <span>{product.count}</span>
+            </div>
+            <div className='d-flex'>
+              <button className='btn btn-danger' onClick={() => addItem(index)}>
+                +
+              </button>
+              <span>{product.count}</span>
+              <button className='btn btn-success' onClick={() => removeItem(index)}>
+                -
+              </button>
+            </div>
+          </div>
         ))
       }
       <span>{totalAmount}</span>
