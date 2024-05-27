a346ccd6592cc8ed8444017fd2f93b970e8ab109
paulawon <166189769+paulawon@users.noreply.github.com>
2024-05-26 21:27:06 -0400

    Add removeItem and addItem functions

--- src/context/PizzeriaContext.jsx
+++ src/context/PizzeriaContext.jsx
@@ -7,12 +7,12 @@ export const PizzeriaProvider = ({ children }) => {
     const [cart, setCart] = useState([]);
 
     async function allPizzas() {
-        const rest = await fetch ("/pizzas.json");
+        const rest = await fetch("/pizzas.json");
         const data = await rest.json();
         setGetPizzas(data);
     }
 
-    const addToCart = ({id,price,name,img}) => {
+    const addToCart = ({ id, price, name, img }) => {
         const indexPizza = cart.findIndex(p => p.id == id)
         console.log(cart, 'cart')
         console.log(indexPizza, 'indexPizza')
@@ -20,16 +20,39 @@ export const PizzeriaProvider = ({ children }) => {
         const product = { id, price, name, img, count: 1 }
         console.log(product, "productContext")
 
-        if(indexPizza >= 0) {
-        //El producto ya existe en el carrito, por lo tanto se incrementa la cantidad del mismo
+        if (indexPizza >= 0) {
+            //El producto ya existe en el carrito, por lo tanto se incrementa la cantidad del mismo
             cart[indexPizza].count++
             setCart([...cart])
-        }else {
-        //El producto no existe, por lo tanto se agrega al carrito
-            setCart([...cart,product])
+        } else {
+            //El producto no existe, por lo tanto se agrega al carrito
+            setCart([...cart, product])
             console.log(cart, 'cart')
         }
         console.log(cart)
+    }
+
+    const addItem = (indexPizza) => {
+        console.log(indexPizza, "indexPizza - addItem")
+        console.log(cart[indexPizza])
+
+        cart[indexPizza].count++
+        console.log(cart[indexPizza])
+        setCart([...cart])
+    }
+
+    const removeItem = (indexPizza) => {
+        console.log(indexPizza, "indexPizza - removeItem")
+        console.log(cart[indexPizza])
+
+        if (cart[indexPizza].count > 1) {
+            cart[indexPizza].count--
+        } else {
+            cart.splice(indexPizza, 1);
+        }
+
+        console.log(cart[indexPizza])
+        setCart([...cart])
     }
 
     useEffect(() => {
@@ -37,8 +60,8 @@ export const PizzeriaProvider = ({ children }) => {
     }, [])
 
     return (
-    <PizzeriaContext.Provider value={{ getPizzas, cart, setGetPizzas, addToCart}}>
-        { children }
-    </PizzeriaContext.Provider>
+        <PizzeriaContext.Provider value={{ getPizzas, cart, setGetPizzas, addToCart, addItem, removeItem }}>
+            {children}
+        </PizzeriaContext.Provider>
     )
 };
